1f4b161c85354814269d4d0e4f50ffd6116fbaa4
Gurbaaz Cheema <gurbaaz.cheema5@gmail.com>
2024-07-27 23:14:33 -0400

    Adding comments...

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        // Prints out "Hello, World!" onto the terminal
         System.out.println("Hello, World!");
     }
 }
